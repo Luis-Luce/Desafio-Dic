2ad1a68029fef335dfeedb8f12b2455e73d87535
Luis <luisluce@gmail.com>
2022-12-24 10:53:29 -0600

    Antes de ser Adaptativo

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
-function encriptar(){
+function encriptar() {
+    // Para facilitar el encriptado se cambian en orden las vocales 
 
-    var frase = document.getElementById("textoEncriptado").value.toLowerCase(); 
+    var texto = document.getElementById("txtInput").value.toLowerCase()
+    var txtCifrado = texto.replace(/e/img, "enter");
+    var txtCifrado = txtCifrado.replace(/o/img, "ober");
+    var txtCifrado = txtCifrado.replace(/i/img, "imes");
+    var txtCifrado = txtCifrado.replace(/a/img, "ai");
+    var txtCifrado = txtCifrado.replace(/u/img, "ufag");
 
-//checa si quitas img son comandos para aceptar minusculas toda la oracion y mayusculas varias lineas 
-
-//checar tambien el manejo de la variable
-    var textoEncriptado = frase.replace(/e/img, "enter");
-    var textoEncriptado = textoEncriptado.replace(/i/img, "imes");
-    var textoEncriptado = textoEncriptado.replace(/a/img, "ai");
-    var textoEncriptado = textoEncriptado.replace(/o/img, "ober");
-    var textoEncriptado = textoEncriptado.replace(/u/img, "ufat");    
-
-    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
-    document.getElementById("botonCopiar").style.display = "show";
-    document.getElementById("botonCopiar").style.display = "inherit";
+    document.getElementById("muneco").style.display = "none";
+    document.getElementById("texto").style.display = "none";  
+    document.getElementById("texto2").style.display ="revert"; 
+    document.getElementById("texto2").innerHTML = txtCifrado;  
+    document.getElementById("btn_copiar").style.display = "show";
+    document.getElementById("btn_copiar").style.display = "inherit";
 }
 
-function desencriptar(){
-var frase = document.getElementById("textoEncriptado").value.toLowerCase(); 
+function desencriptar() {
+    //mismo trabajo que el encriptado pero a la inversa
+    var texto = document.getElementById("txtInput").value.toLowerCase()
+    var txtCifrado = texto.replace(/enter/img, "e");
+    var txtCifrado = txtCifrado.replace(/ober/img, "o");
+    var txtCifrado = txtCifrado.replace(/imes/img, "i");
+    var txtCifrado = txtCifrado.replace(/ai/img, "a");
+    var txtCifrado = txtCifrado.replace(/ufag/img, "u");
 
-//checa si quitas img son comandos para aceptar minusculas toda la oracion y mayusculas varias lineas 
-
-//checar tambien el manejo de la variable
-    var textoEncriptado = frase.replace(/enter/img, "e");
-    var textoEncriptado = textoEncriptado.replace(/imes/img, "i");
-    var textoEncriptado = textoEncriptado.replace(/ai/img, "a");
-    var textoEncriptado = textoEncriptado.replace(/ober/img, "o");
-    var textoEncriptado = textoEncriptado.replace(/ufat/img, "u");    
-
-    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+    //declaro el comportamiento dinamico de las celdas
+    document.getElementById("muneco").style.display = "none";
+    document.getElementById("texto").style.display = "none"; 
+    document.getElementById("texto2").style.display ="revert"; 
+    document.getElementById("texto2").innerHTML = txtCifrado;  
+    document.getElementById("btn_copiar").style.display = "show";
+    document.getElementById("btn_copiar").style.display = "inherit";
 }
 
-function copiar(){
-    var contenido = document.querySelector("#textoDesencriptado");
+function copiar() {
+    //como se comporta el boton de copiar
+    var contenido = document.querySelector("#texto2");
     contenido.select();
     document.execCommand("copy");
+    document.getElementById("muneco").style.display = "revert";
+    document.getElementById("texto").style.display = "revert";
+    document.getElementById("texto2").style.display ="none";  
+    document.getElementById("btn_copiar").style.display = "none";
+    document.getElementById("btn_copiar").style.display = "none";
+
 }
